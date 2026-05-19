6093b42ed26cc7457467dabb6864dd6b5ac451e9
Roberto Delgado <216353140+delgadoroberto@users.noreply.github.com>
2026-05-19 00:46:55 -0300

    Add suspicious IP detection

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,26 @@
+from collections import Counter
+import re
+
 failed_attempts = 0
+ips = []
 
 with open("sample_logs.txt", "r") as file:
     for line in file:
+
         if "Failed password" in line:
             failed_attempts += 1
 
-print(f"Failed login attempts: {failed_attempts}")
+            match = re.search(r'from (\d+\.\d+\.\d+\.\d+)', line)
+
+            if match:
+                ips.append(match.group(1))
+
+print(f"\nFailed login attempts: {failed_attempts}")
+
+counter = Counter(ips)
+
+print("\nSuspicious IPs:")
+
+for ip, count in counter.items():
+    if count >= 2:
+        print(f"{ip} -> {count} failed attempts")
